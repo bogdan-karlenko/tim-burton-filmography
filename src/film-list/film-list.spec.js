9ee5acb4a75949588bbc1c9699b5cd1401bd319c
Bogdan Karlenko <opposite.ps@gmail.com>
2017-10-03 19:55:22 +0300

    full film list and bootstrap fix

--- src/film-list/film-list.spec.js
+++ src/film-list/film-list.spec.js
@@ -1,10 +1,37 @@
 describe('filmList', function() {
 
-  beforeEach(module('filmographyApp'));
+  beforeEach(module('filmList'));
 
-describe('list controller', function() {
-  it('should create a film list with', function() {
+  // Test the controller
+  describe('FilmListController', function() {
+    var $httpBackend, ctrl;
 
+    beforeEach(inject(function($componentController, _$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('/src/films/films.json')
+        .respond([{
+          name: '9'
+        }, {
+          name: 'Batman'
+        }]);
+
+      ctrl = $componentController('filmList');
+    }));
+
+    it('should create a `films` property with 2 films fetched with `$http`', function() {
+      expect(ctrl.films).toBeUndefined();
+
+      $httpBackend.flush();
+      expect(ctrl.films).toEqual([{
+        name: '9'
+      }, {
+        name: 'Batman'
+      }]);
+    });
+
+    it('should set a default value for the `orderProp` property', function() {
+      expect(ctrl.orderProp).toBe('age');
+    });
   });
-});
+
 });
